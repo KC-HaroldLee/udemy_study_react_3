6eb3c4c38acc58edeb2f13fb7a1606ce122bfca6
KC-HaroldLee <kklastchnpd@naver.com>
2024-08-09 12:48:52 +0900

    섹션3 완료

--- section03/src/App.jsx
+++ section03/src/App.jsx
@@ -9,14 +9,14 @@ import { useState } from "react"; // use~는 Hooks,
 import {EXAMPLES} from './data.js'
 
 
-
 // useState() // 이건 안되는 건가??????
 function App() {
   // const stateArray = useState('Plz clck btn') 
   // const [ selectedTopic, setSelectedTopic ] = useState('Plz clck btn') 
-  const [ selectedTopic, setSelectedTopic ] = useState('components') 
-  console.log (selectedTopic)
-  console.log (setSelectedTopic)
+  // const [ selectedTopic, setSelectedTopic ] = useState('components')  // 초기값이 있어야했다.
+  const [ selectedTopic, setSelectedTopic ] = useState('') 
+  // console.log (selectedTopic)
+  // console.log (setSelectedTopic)
   // 앞에껀 변수 뒤에껀 함수(글씨 색이 다름)
 
   // let tabContent = 'plz click a button'
@@ -28,6 +28,21 @@ function App() {
     setSelectedTopic(selectedButton)
     console.log('work?', selectedTopic)
   }
+
+let tabContent = <p>Plz Slct a tpc</p>
+
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>
+  }
+
   return (
     <div>
       <Header />
@@ -35,10 +50,14 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            {CORE_CONCEPTS.map((conceptItem) => 
+              <CoreConcept key={conceptItem.title}{...conceptItem}></CoreConcept>) // 억지로라도 key prob을 줘야한다.
+            }
+            {/* 아래를 위 처럼 반복문으로 만듦 */}
+            {/* <CoreConcept {...CORE_CONCEPTS[0]}/>
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/> */}
             {/* 아래와 같이 하면 오래 걸린다. (뭐 어차피 반복문 돌리겠지만) */}
             {/* <CoreConcept 
               title={CORE_CONCEPTS[0].title} 
@@ -52,14 +71,24 @@ function App() {
             {/* 함수의 포인트를 넘긴다는 느낌으로 기억하자 */}
             {/* <TabButton onSelect={handleSelected}>Component!</TabButton>  */}
             {/* 문제는 위처럼 하면 함수 자체에 인자를 넘길 수 없다는 단점이 있다. */}
-            <TabButton onSelect={() => handleSelected('components')}>Component!</TabButton> 
-            <TabButton onSelect={() => handleSelected('jsx')}>JSX!</TabButton>
-            <TabButton onSelect={() => handleSelected('props')}>Probs!</TabButton>
-            <TabButton onSelect={() => handleSelected('state')}>State!</TabButton>
+            <TabButton isSelected={selectedTopic==='components'} onSelect={() => handleSelected('components')}>Component!</TabButton> 
+            <TabButton isSelected={selectedTopic==='jsx'} onSelect={() => handleSelected('jsx')}>JSX!</TabButton>
+            <TabButton isSelected={selectedTopic==='props'} onSelect={() => handleSelected('props')}>Probs!</TabButton>
+            <TabButton isSelected={selectedTopic==='state'} onSelect={() => handleSelected('state')}>State!</TabButton>
             {/* 이런 방법도 있다느 정도만 */}
           </menu>
-          {/* {tabContent} */}
-          <div id="tab-content">
+
+          {tabContent}
+          {/* 이방법 말고 상위로 올림 */}
+          {/* {!selectedTopic // 삼항 연산자를 사용할 경우
+                ? <p>Plz Slct a tpc</p> 
+                : null} */}
+          {/* {!selectedTopic &&
+                 <p>Plz Slct a tpc</p>}
+
+          {selectedTopic &&
+            <div id="tab-content">
+              {selectedTopic}
               <h3>{EXAMPLES[selectedTopic].title}</h3>
               <p>{EXAMPLES[selectedTopic].description}</p>
               <pre>
@@ -67,7 +96,7 @@ function App() {
                   {EXAMPLES[selectedTopic].code}
                 </code>
               </pre>
-          </div>
+          </div>} */}
 
         </section>
       </main>
--- section03/src/components/TabButton.jsx
+++ section03/src/components/TabButton.jsx
@@ -1,6 +1,6 @@
 // 여기서부터 시작
 
-function TabButton({children, onSelect}) {
+function TabButton({children, onSelect, isSelected}) { // className을 동적으로 설정하기 위해 isSelected추가함
     // 클릭 이벤트
     // document.querySelector('button').addEventListener('click', () => {
     // })
@@ -11,7 +11,7 @@ function TabButton({children, onSelect}) {
 
     return <>
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li> 
     </>
 }
